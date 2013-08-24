0c694fe53646e26c8f48c4180555facb1c11c222
ElliotFiske <elliotfiske@gmail.com>
2013-08-23 20:25:28 -0700

    Added Box2D

--- init.py
+++ init.py
@@ -1,5 +1,5 @@
 import pygame, math, random, sys, os, hashlib
-import Box2D
+from Box2D import *
 import time
 from pygame.locals import *
 
@@ -10,15 +10,15 @@ pygame.display.set_caption("Project Hiatus")
 clock = pygame.time.Clock()
 
 def load_image(name, colorkey=None):
-	fullname = os.path.join('img', name)
-	try:
-		image = pygame.image.load(fullname)
-	except pygame.error, message:
-		print 'Cannot load image:', name
-		raise SystemExit, message
-	image = image.convert()
-	if colorkey is not None:
-		if colorkey is -1:
-			colorkey = image.get_at((0,0))
-		image.set_colorkey(colorkey, RLEACCEL)
-	return image, image.get_rect()
+    fullname = os.path.join('img', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error, message:
+        print 'Cannot load image:', name
+        raise SystemExit, message
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0,0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
--- main.py
+++ main.py
@@ -22,20 +22,39 @@ exec(open('arena_mechanics.py'))
 exec(open('character_class.py'))
 
 def init():
-	global font				# Font
-	global player1, player2	# Players
-	global currentArena		# Midpoint
-	global camera			# duh
-	global Import_sprite,interface		# RenderPlains
-
-	font = pygame.font.Font("fonts/ka1.ttf", 30)
-	
-	player1 = Player(1, 8)
-	player2 = Player(1, 9)
-	currentArena = 8.5
-	
-	camera = Camera(currentArena)
-	
+    global font                # Font
+    global player1, player2    # Players
+    global currentArena        # Midpoint
+    global camera            # duh
+    global Import_sprite,interface        # RenderPlains
+    global shapes           # all the Box2D objects
+    global world            # the Box2D world
+    
+    font = pygame.font.Font("fonts/ka1.ttf", 30)
+    
+    player1 = Player(1, 8)
+    player2 = Player(1, 9)
+    currentArena = 8.5
+    
+    camera = Camera(currentArena)
+    # Init physics "world", defining gravity. doSleep means that if an object
+    # comes to rest, it can "sleep" and be ignored by the physics engine for a bit.
+    world = b2World(gravity=(0, -10), doSleep = True)
+    
+    # Initialize the ground. Will also need to initialize obstacles/arena
+    # components here later on.
+    ground = world.CreateStaticBody(
+        position = (0, -10),
+        shapes = b2PolygonShape(box = (50,10))
+    )
+    
+    body = world.CreateDynamicBody(position = (0,4))
+    box = body.CreatePolygonFixture(box = (1,1), density = 1, friction = 0.3)
+    
+    shapes = {}
+    shapes["box"] = body
+    shapes["ground"] = ground
+    
 # -----------------------------------------------------------------------|
 # -----------------------------------------------------------------------|
 #                                  MAIN LOOP                             |
@@ -43,21 +62,33 @@ def init():
 # -----------------------------------------------------------------------|
 init()
 while 1:
-	# USER INPUT
-	deltat = clock.tick(50)
-	
-	# Check user input
-	for event in pygame.event.get():
-		if event.type is pygame.QUIT: sys.exit()
-		if hasattr(event, 'key'):
-			if event.key is K_ESCAPE: 
-				if event.type is pygame.KEYDOWN: sys.exit()
-			if event.key is K_a:
-				if event.type is pygame.KEYDOWN: changeArena(currentArena - 1)
-			if event.key is K_d:
-				if event.type is pygame.KEYDOWN: changeArena(currentArena + 1)
-				
-	camera.update()
-	camera.draw(screen)
-						
-	pygame.display.flip()
+    # USER INPUT
+    sleeptime = 1000 / 60
+    deltat = clock.tick(sleeptime)
+    
+    # Update a "tick" in physics land
+    world.Step(sleeptime, 10, 10)
+    
+    # Reset forces for the next frame
+    world.ClearForces()
+    
+    # Draw dem boxes
+    for shape in shapes:
+        print("drawing shapes yo")
+         
+    
+    # Check user input
+    for event in pygame.event.get():
+        if event.type is pygame.QUIT: sys.exit()
+        if hasattr(event, 'key'):
+            if event.key is K_ESCAPE: 
+                if event.type is pygame.KEYDOWN: sys.exit()
+            if event.key is K_a:
+                if event.type is pygame.KEYDOWN: changeArena(currentArena - 1)
+            if event.key is K_d:
+                if event.type is pygame.KEYDOWN: changeArena(currentArena + 1)
+                
+    camera.update()
+    camera.draw(screen)
+                        
+    pygame.display.flip()
